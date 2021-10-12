050fc3a43effbb9f5b9c931cd15f1f686a8ed993
Jose Espinosa <jhespinosa@gmail.com>
2021-10-11 23:17:56 -0700

    Work

--- id3.py
+++ id3.py
@@ -12,14 +12,19 @@ import argparse
 
 eyed3.log.setLevel("ERROR")
 
-PATH = "/home/jose/Music/tmp"
+#PATH = "/home/jose/tmp"
+#PATH = "/home/jose/Music/tmp"
 #PATH = '/home/jose/Downloads/Podcast'
+PATH="/home/jose/Downloads/Podcast1"
 
 P1 = re.compile("^(.+): (.+)$")
-P2 = re.compile("^(00 - )?(.+) ?- ?(.+)(\.mp3)?$")
-P3 = re.compile("^(.+): (?:[\"\“]?)([^\"\”]+)(?:[\"\”]?)(.*)$")
-P4 = re.compile("^(.+) ??- ?(.+)(?:\.mp3)$")
+P2 = re.compile("^(00 - )?(.+) ?- ?(.+?)(\.mp3)?$")
+P3 = re.compile("^'?(.+)'?: (?:[\"\“]?)([^\"\”]+)(?:[\"\”]?)(.*)$")
+#P4 = re.compile("^(.+) ??- ?(.+)(?:\.mp3)$")
+P4 = re.compile("^(.+)  -  (.+)$")
+P41 = re.compile("^(.+) ?- ?(.+)$")
 P5 = re.compile("^\\d{8}_[a-z_]+_128.mp3$")
+P6 = re.compile("^(.+) - (.+)_\d+\.mp3$")
 
 G_PODCAST = eyed3.id3.Genre("Podcast")
 G_SLASH   = eyed3.id3.Genre("/")
@@ -33,8 +38,12 @@ N = 0
 M = 20
 
 def save(self, filename=None, version=None, encoding=None, backup=False, preserve_file_time=False, max_padding=None):
+#		if (not self.artist or not self.title):
+#			print(self.artist, self.title, self.fname)
 	try:
-		self.oldsave(filename, version, encoding,backup, preserve_file_time, max_padding)
+
+			self.oldsave(filename, version, encoding,backup, preserve_file_time, max_padding)
+
 	except:
 		try:
 			if(self.version):
@@ -43,7 +52,7 @@ def save(self, filename=None, version=None, encoding=None, backup=False, preserv
 				version = eyed3.id3.ID3_ANY_VERSION
 			self.oldsave(filename, version, encoding, backup, preserve_file_time, max_padding)
 		except Exception as e:
-			print(";(", e, version) 
+			print(";(", e, version)
 
 eyed3.id3.tag.Tag.oldsave = eyed3.id3.tag.Tag.save
 eyed3.id3.tag.Tag.save = save
@@ -52,7 +61,6 @@ def process_song(fname, name = None):
 	a = eyed3.load(fname)
 
 	if(not a or not a.tag):
-		print(1)
 		return
 
 	a.tag.fname = fname
@@ -65,18 +73,16 @@ def process_song(fname, name = None):
 			a.tag.genre  = G_NONE
 			a.tag.save()
 			return
-		print(2)
 	if((a.tag.genre and a.tag.genre == G_PODCAST and a.tag.title) or
 		(a.tag.genre and not a.tag.genre == G_NONE and a.tag.title)):
 		m = P2.match(a.tag.title)
 		if(m):
-			print(1, m.group())
+			print(title)
 			a.tag.artist = m.group(1)
 			a.tag.title  = m.group(2)
 			a.tag.genre  = G_NONE
 			a.tag.save()
 			return
-		print(3)
 	if(a.tag.artist and (a.tag.artist == "KCRW" or a.tag.artist == "KEXP") and a.tag.title):
 		m = P3.match(a.tag.title)
 		if(m):
@@ -87,7 +93,6 @@ def process_song(fname, name = None):
 				a.tag.title  = m.group(2)
 			a.tag.save()
 			return
-		print(4)
 	if(a.tag.artist and a.tag.artist == "KEXP" and a.tag.title):
 		m = P4.match(a.tag.title)
 		if(m):
@@ -96,7 +101,6 @@ def process_song(fname, name = None):
 			a.tag.genre  = G_NONE
 			a.tag.save()
 			return
-		print(5)
 	if(a.tag.artist and a.tag.artist == "MPR" and a.tag.title):
 		m = P4.match(a.tag.title)
 		if(m):
@@ -105,12 +109,26 @@ def process_song(fname, name = None):
 			a.tag.genre  = G_NONE
 			a.tag.save()
 			return
-		print(6)
+	if(a.tag.artist and a.tag.artist == "MPR" and a.tag.title):
+		m = P41.match(a.tag.title)
+		if(m):
+			a.tag.artist = m.group(1)
+			a.tag.title  = m.group(2)
+			a.tag.genre  = G_NONE
+			a.tag.save()
+			return
 	if(name and P5.match(name)):
-		#print(7)
-		pass
+		return
+	if(name):
+		m = P6.match(name)
+		if(m):
+			a.tag.artist = m.group(1)
+			a.tag.title  = m.group(2)
+			a.tag.genre  = G_NONE
+			a.tag.save()
+			return
 	else:
-		print(8, a.tag.fname)
+		pass
 
 def onError(e):
 	print("OnError:",e)
@@ -123,7 +141,7 @@ def dir_path(string):
 
 def setup_args():
 	parser = argparse.ArgumentParser(description='Id3 tags parser')
-	parser.add_argument("directory", nargs='?', type=dir_path, default="/home/jose/Music/tmp", help='Directory where files to parse are')
+	parser.add_argument("directory", nargs='?', type=dir_path, default=PATH, help='Directory where files to parse are')
 	args = parser.parse_args()
 	return args
 
@@ -137,8 +155,6 @@ def traverse(path):
 				i = i + 1
 				if(i>N):
 					process_song("/".join([dirName, fname]),fname)
-				if(i % M == 0):
-					print(psutil.Process().num_fds(), gc.get_stats(), i)
 
 
 if __name__ == "__main__":
